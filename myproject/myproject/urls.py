b8518813bc6e1d1603a958fb2abecb752bb4299f
Jun Yan <junyan@Jun-Yans-MacBook.local>
2014-09-21 17:17:09 -0600

    minimum upload function from https://github.com/axelpale/minimal-django-file-upload-example

---
+++ myproject/myproject/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+
+urlpatterns = patterns('',
+                       url(r'^myapp/', include('myapp.urls'))
+                       )
+
+
